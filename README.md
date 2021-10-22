e34365131603cf2fc1874eeb4955b0631761e2b8
xingwangqiao <xingwangqiao@163.com>
2021-10-23 00:31:48 +0800

    newfile: designed class of Any used to manage all of the shared ptr

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cpp_basic_tools
+使用 c++ 实现一些常用的工具
---
+++ any.h
@@ -0,0 +1,113 @@
+/**
+ * @brief 使用统一的类型用于管理频繁出现的 std::shared_ptr
+ * 
+ */
+#include <memory>
+
+namespace basic {
+class Any {
+public:
+    Any() = default;
+    ~Any() = default;
+
+    Any(std::nullptr_t) : Any() {}
+
+    Any(const Any& rhs) : m_ptr_(rhs.m_ptr_) { }
+    Any(Any &&rhs) : m_ptr_(std::move(rhs.m_ptr_)) { }
+
+    template <typename T>
+    Any(std::shared_ptr<T> rhs) :m_ptr_(std::static_pointer_cast<void>(rhs)) { }
+
+    template <typename T>
+    Any(T* ptr) : m_ptr_(std::shared_ptr<void>((void*)ptr)){ }
+
+    Any& operator= (const Any& rhs) {
+        if(&rhs == this)
+            return *this;
+        this->m_ptr_ = rhs.m_ptr_;
+        return *this;
+    } 
+    Any& operator= (Any&& rhs) {
+        this->m_ptr_ = std::move(rhs.m_ptr_);
+        return *this;
+    }
+
+    bool operator== (const Any& rhs) {
+        return this->m_ptr_ == rhs.m_ptr_;
+    }
+    bool operator!= (const Any& rhs) {
+        return !(*this == rhs);
+    }
+
+    template <typename T>
+    bool operator== (std::shared_ptr<T> rhs) {
+        return this->m_ptr_ == std::static_pointer_cast<void>(rhs);
+    }
+    template <typename T>
+    bool operator!= (std::shared_ptr<T> rhs) {
+        return !(*this == rhs);
+    }
+    
+    template <typename T>
+    bool operator== (T* rhs) {
+        return this->m_ptr_.get() == (void*)(rhs);
+    }
+    template <typename T>
+    bool operator!= (T* rhs) {
+        return !(*this == rhs);
+    }
+
+    operator bool () {
+        return m_ptr_ != nullptr;
+    }
+
+    template <typename T>
+    std::shared_ptr<T> get() {
+        return std::static_pointer_cast<T>(m_ptr_);
+    }
+
+    void reset() {
+        m_ptr_.reset();
+    }
+
+protected:
+    std::shared_ptr<void> m_ptr_;
+};
+}
+
+class class_A{
+
+};
+
+int main() {
+    int err = 0;
+    basic::Any test1();
+    if(test1){
+        return -1;
+    }
+
+    basic::Any test2(nullptr);
+    if(test2){
+        return -2;
+    }
+
+    int* int_test3 = new int(0);
+    basic::Any test3(int_test3);
+    if(!test3 || (test3 != int_test3)){
+        err = -3;
+    }
+    delete int_test3;
+    if(err != 0){
+        return err;
+    }
+
+    do {
+    std::shared_ptr<class_A> class_test4 = std::make_shared<class_A>();
+    basic::Any test4(class_test4);
+    if(!test4 || (test4 != class_test4)){
+        return -4;
+    }
+    }while (false);
+
+    return 0;
+}
