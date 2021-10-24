9689c75995d20d9bbbcf1ba11f46c9a518bd792d
qiaoxingwang <qiaoxingwang@bytedance.com>
2021-10-24 11:15:44 +0800

    test: compelete basic::Any unit test

--- test/basic_any_unit_test.cc
+++ test/basic_any_unit_test.cc
@@ -24,6 +24,11 @@ TEST_F(BasicAnyUnitTest, ConstructionByDefault) {
     EXPECT_EQ(nullptr, any.get<void>());
 }
 
+TEST_F(BasicAnyUnitTest, ConstructionByNullptr) {
+    basic::Any any(nullptr);
+    EXPECT_EQ(nullptr, any.get<int>().get());
+}
+
 TEST_F(BasicAnyUnitTest, ConstructionByPointer) {
     // create pointer of int;
     int* int_ptr = new int(0);
@@ -36,4 +41,36 @@ TEST_F(BasicAnyUnitTest, ConstructionBySharedPointer) {
     basic::Any any(int_ptr);
     EXPECT_EQ(int_ptr, any.get<int>());
 }
+
+TEST_F(BasicAnyUnitTest, Compare) {
+    basic::Any any;
+    EXPECT_TRUE(any == nullptr);
+
+    basic::Any any_null(nullptr);
+    EXPECT_TRUE(any_null == nullptr);
+
+    int* ptr = new int(0);
+    basic::Any any_ptr(ptr);
+    EXPECT_TRUE(any_ptr == ptr);
+
+    std::shared_ptr<int> s_ptr = std::make_shared<int>(0);
+    basic::Any any_shared_ptr(s_ptr);
+    EXPECT_TRUE(any_shared_ptr == s_ptr);
+}
+
+TEST_F(BasicAnyUnitTest, Assignment) {
+    basic::Any any;
+
+    any = nullptr;
+    EXPECT_TRUE(any == nullptr);
+
+    int* ptr = new int(0);
+    any = ptr;
+    EXPECT_TRUE(any == ptr);
+
+    std::shared_ptr<int> s_ptr = std::make_shared<int>(0);
+    any = s_ptr;
+    EXPECT_TRUE(any == s_ptr);
+}
+
 }
