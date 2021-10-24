0f9d6d6ce903ddff8d0da4fe64addc3feb398669
qiaoxingwang <qiaoxingwang@bytedance.com>
2021-10-24 23:33:59 +0800

    newfile: design data struction to manange instance resource

---
+++ instances_resource_manager.h
@@ -0,0 +1,33 @@
+/**
+ * @brief manage resources of instance just like pointer
+ */
+#include "any.h"
+
+#include <vector>
+#include <map>
+
+// tmp define
+#define InstanceResources int
+#define InstanceID int
+// end tmp define
+
+namespace basic {
+class InstancesResourceManager {
+public:
+    InstancesResourceManager() = default;
+    ~InstancesResourceManager() = default;
+
+    InstanceID createStorage();
+    void destroyStorage(InstanceID);
+
+    std::shared_ptr<InstanceResources> getResources(InstanceID id);
+    // stored resource that no instanceID
+    std::shared_ptr<InstanceResources> getAnyResources();
+
+    std::vector<InstanceID> getInstanceIDOfAllResource(void* target, int key = 0);
+    InstanceID getInstanceIDOfFirstResource(void* target, int key = 0);
+};
+}
+/**
+ * map<InstanceID, InstanceResources> resources;
+ */
